7ca297f18ddb16e7b3a790e7fb8812ffe50b474a
elio987 <leo_gracidam@hotmail.com>
2022-04-13 17:04:51 -0500

    second commit

--- build/catkin_generated/installspace/_setup_util.py
+++ build/catkin_generated/installspace/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/elio987/ROS_listener_talker_turtle/devel;/home/elio987/mobile-robots-module/ros/catkin_odometri/devel;/home/elio987/catkin_ws/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/elio987/ROS_lis_tal_tur/devel;/home/elio987/ROS_listener_talker_turtle/devel;/home/elio987/mobile-robots-module/ros/catkin_odometri/devel;/home/elio987/catkin_ws/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/elio987/ROS_listener_talker_turtle/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH='/home/elio987/ROS_listener_talker_turtle/devel:/home/elio987/mobile-robots-module/ros/catkin_odometri/devel:/home/elio987/catkin_ws/devel:/opt/ros/melodic'
+export LD_LIBRARY_PATH='/home/elio987/ROS_listener_talker_turtle/devel/lib:/home/elio987/mobile-robots-module/ros/catkin_odometri/devel/lib:/home/elio987/catkin_ws/devel/lib:/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/home/elio987/ROS_listener_talker_turtle/devel/lib/pkgconfig:/home/elio987/mobile-robots-module/ros/catkin_odometri/devel/lib/pkgconfig:/home/elio987/catkin_ws/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/elio987/ROS_listener_talker_turtle/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/elio987/ROS_listener_talker_turtle/devel/share/common-lisp:/home/elio987/mobile-robots-module/ros/catkin_odometri/devel/share/common-lisp:/home/elio987/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/elio987/ROS_listener_talker_turtle/src:/home/elio987/mobile-robots-module/ros/catkin_odometri/src:/home/elio987/catkin_ws/src:/opt/ros/melodic/share'
